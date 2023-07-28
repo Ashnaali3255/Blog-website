26d23e0a388556a07ae9f3167db10c601fe5b754
Ashnaali3255 <ashnaalipk@gmail.com>
2023-07-28 21:31:19 +0500

    Fix problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :photo, :bio, :user_id)
+      params.require(:user).permit(:name, :photo, :bio)
     end
 end
