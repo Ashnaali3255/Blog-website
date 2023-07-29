376ea754c3467057b767d392d5899ccc0bf35bdd
Ashnaali3255 <ashnaalipk@gmail.com>
2023-07-30 04:46:37 +0500

    update test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Post, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_most(250) }
     it { should validate_presence_of(:comments_counter) }
     it { should validate_numericality_of(:comments_counter).only_integer.is_greater_than_or_equal_to(0) }
     it { should validate_presence_of(:likes_counter) }
