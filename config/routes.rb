1d73b61c595662285b4f2af403014af6eba742ae
Ashnaali3255 <ashnaalipk@gmail.com>
2023-08-01 20:37:29 +0500

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :likes
   resources :comments
-  resources :posts
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  # Add more routes as needed
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # root "controller#action" - Uncomment and modify this line to define the root path route
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_28_145835) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_01_083302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_28_145835) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
