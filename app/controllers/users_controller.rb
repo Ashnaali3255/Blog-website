4f598b11e14f96d03e7bf6a92521da1814069bba
Ashnaali3255 <ashnaalipk@gmail.com>
2023-08-11 14:51:03 +0500

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,2 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.includes(posts: [:comments]).find(params[:id])
-    @most_recent_posts = @user.recent_posts
-  end
-
-  def index
-    @users = User.all
-  end
 end
